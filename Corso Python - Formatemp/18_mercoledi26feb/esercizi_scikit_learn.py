73f991eff7895dfc377600b2626642a4c10844fa
Astelen <astelen@hotmail.com>
2025-02-26 11:34:04 +0100

    Esercizi machine learning with scikit_learn

---
+++ Corso Python - Formatemp/18_mercoledi26feb/esercizi_scikit_learn.py
@@ -0,0 +1,147 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+#Loading data
+data = load_iris()
+X = data.data
+y = data.target
+
+#Splitting test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=8)
+
+#KNN model
+#Creo istanza modello
+knn = KNeighborsClassifier(n_neighbors=3)
+#Addestro il modello con fit sui dati di training
+knn.fit(X_train, y_train)
+#Effettuo predizioni sui nuovi dati
+predictions = knn.predict(X_test)
+
+#Accuracy per valutare le prestazioni
+accuracy = accuracy_score(y_test, predictions)
+
+print(accuracy)
+#Accuracy 0.9
+
+
+
+# Riduciamo il test_size per verificare l'accuratezza
+#Loading data
+data = load_iris()
+X = data.data
+y = data.target
+
+#Splitting test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=10)
+
+#KNN model
+#Creo istanza modello
+knn = KNeighborsClassifier(n_neighbors=3)
+#Addestro il modello con fit sui dati di training
+knn.fit(X_train, y_train)
+#Effettuo predizioni sui nuovi dati
+predictions = knn.predict(X_test)
+
+#Accuracy per valutare le prestazioni
+accuracy = accuracy_score(y_test, predictions)
+print(accuracy)
+#Accuracy 0.93
+
+
+
+
+#Aumentiamo il test_size per verificare l'accuratezza
+#Loading data
+data = load_iris()
+X = data.data
+y = data.target
+
+#Splitting test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=10)
+
+#KNN model
+#Creo istanza modello
+knn = KNeighborsClassifier(n_neighbors=3)
+#Addestro il modello con fit sui dati di training
+knn.fit(X_train, y_train)
+#Effettuo predizioni sui nuovi dati
+predictions = knn.predict(X_test)
+
+#Accuracy per valutare le prestazioni
+accuracy = accuracy_score(y_test, predictions)
+print(accuracy)
+#Accuracy 0.96
+
+
+
+#Provo a cambiare anche n. neighbors =5
+data = load_iris()
+X = data.data
+y = data.target
+
+#Splitting test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+
+#KNN model
+#Creo istanza modello
+knn = KNeighborsClassifier(n_neighbors=5)
+#Addestro il modello con fit sui dati di training
+knn.fit(X_train, y_train)
+#Effettuo predizioni sui nuovi dati
+predictions = knn.predict(X_test)
+
+#Accuracy per valutare le prestazioni
+accuracy = accuracy_score(y_test, predictions)
+print(accuracy)
+#Accuracy 0.96
+
+
+
+
+#Provo a cambiare anche n. neighbors =10
+data = load_iris()
+X = data.data
+y = data.target
+
+#Splitting test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+
+#KNN model
+#Creo istanza modello
+knn = KNeighborsClassifier(n_neighbors=10)
+#Addestro il modello con fit sui dati di training
+knn.fit(X_train, y_train)
+#Effettuo predizioni sui nuovi dati
+predictions = knn.predict(X_test)
+
+#Accuracy per valutare le prestazioni
+accuracy = accuracy_score(y_test, predictions)
+print(accuracy)
+#Accuracy 1
+
+
+
+
+
+#Provo a cambiare anche n. neighbors =10, test_size = 40
+data = load_iris()
+X = data.data
+y = data.target
+
+#Splitting test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=10)
+
+#KNN model
+#Creo istanza modello
+knn = KNeighborsClassifier(n_neighbors=10)
+#Addestro il modello con fit sui dati di training
+knn.fit(X_train, y_train)
+#Effettuo predizioni sui nuovi dati
+predictions = knn.predict(X_test)
+
+#Accuracy per valutare le prestazioni
+accuracy = accuracy_score(y_test, predictions)
+print(accuracy)
+#Accuracy 0.96
